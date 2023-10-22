81e7e70318f66dc28f7e59cefca577f075a0661f
Dante Castro Garro <profe.paiche@gmail.com>
2023-10-22 18:00:45 +0200

    gridMaker: Generalized version for lonlat points
    Now the code needs the longitude and latitude inputs as a sfc_POINTS
    grid. This makes it easier to work with non-regular distributed grids,
    like the ones from CORDEX. The functionality should be the same for
    regular-spaced grids.
    
    Also, to create the sfc_POINTS object from the original coordinates,
    please use the "coords2sf" new function.
    
    NOTE: The code was updated as well to improve readability

--- R/gridMaker.R
+++ R/gridMaker.R
@@ -2,100 +2,80 @@
 #'
 #' This function takes each grid from longitude and latitude vectors or a raster
 #' image and creates polygons wich later can be used with sf package functions.
-#' 
-#' @param rtr Raster image from raster package.
-#' @param lon Numeric vector with longitude values.
-#' @param lat Numeric vector with latitude values.
-#' @param type String. If "grid" selected, it will take the values from lon and 
-#'  lat to create the polygons. If "raster", it will use the raster dimensions.
-#' @param scr Integer. EPSG code for polygons.
+#'
+#' @param grid Raster image or points (in a grid configuration, from lower-left
+#'  to uper-right).
+#' @param crs Integer or String. EPSG code or PROJ4 string for polygons.
 #' @param export Logic. Save the polygons in file.
 #' @param name String. Name of the file to be exported.
 #'
-#' @return polygons.
+#' @return Grid as polygons
 #'
 #' @export
 
-gridMaker = function(rtr = NULL, lon = NULL, lat = NULL, 
-                     type = "grid", scr = 4326, 
-                     export = FALSE, name = "malla.shp") {
-	
-	# Checking data type 
-    # Getting latitude and longitude data
+gridMaker = function(grid, crs = 4326, export = FALSE, name = "malla.shp") {
+    # Getting corners of the boundary arean and number of grids cells
+    type = class(grid)
+    if (any(type == "sfc_POINT")) {
+        ndims = dimensionSize(grid)
+        nc = ndims[1] * ndims[2]
+        grid_df = grid |> sf::st_coordinates() |> as.data.frame()
+        lon_res = grid_df$X[2] - grid_df$X[1]
+        lat_res = grid_df$Y[ndims[1] + 1] - grid_df$Y[1]
+        lon_l = grid_df$X[1] - lon_res / 2
+        lon_r = grid_df$X[nc] + lon_res / 2
+        lat_d = grid_df$Y[1] - lat_res / 2
+        lat_u = grid_df$Y[nc] + lat_res / 2
+    } else if (type == "raster") {
+        # FIXME: There should be a better way to obtain the dimension size
+        lon_res = raster::xres(grid)
+        lat_res = raster::yres(grid)
+        lon_l = raster::xmin(grid)
+        lon_r = raster::xmax(grid)
+        lat_d = raster::ymin(grid)
+        lat_u = raster::ymax(grid)
+        lon_l[lon_l > 180] = lon_l[lon_l > 180] - 360
+        lon_r[lon_r > 180] = lon_r[lon_r > 180] - 360
+        lon = seq(lon_l + lon_res / 2, lon_r - lon_res / 2, lon_res)
+        lat = seq(lat_d + lat_res / 2, lat_u - lat_res / 2, lat_res)
+        ndims = c(length(lon), length(lat))
+    } else {
+        stop("input class must be either 'sfc_POINT' or 'raster'")
+    }
+    corners = c(
+        lon_l, lat_u,
+        lon_r, lat_u,
+        lon_r, lat_d,
+        lon_l, lat_d,
+        lon_l, lat_u
+    )
 
-	if (type == "grid") {
-
-		if(is.null(lon) | is.null(lat)) stop("Missing lat and lon data")
-
-		lon[lon > 180] = lon[lon > 180] - 360
-		lon = sort(lon)
-
-		lon.res = diff(lon)[1]
-		lat.res = diff(lat)[1]
-		lon.l = min(lon) - lon.res / 2
-		lon.r = max(lon) + lon.res / 2
-		lat.d = min(lat) - lat.res / 2
-		lat.u = max(lat) + lat.res / 2
-
-	} else if (tipo == "raster") {
-
-		if (class(rtr) != "RasterLayer") stop("Data is not RasterLayer class")
-
-		lon.res = raster::xres(rtr)
-		lat.res = raster::yres(rtr)
-		lon.l = raster::xmin(rtr)
-		lon.r = raster::xmax(rtr)
-		lat.d = raster::ymin(rtr)
-		lat.u = raster::ymax(rtr)
-
-		lon.l[lon.l > 180] = lon.l[lon.l > 180] - 360
-		lon.r[lon.r > 180] = lon.r[lon.r > 180] - 360
-
-		lon = seq(lon.l + lon.res/2, lon.r - lon.res/2, lon.res)
-		lat = seq(lat.d + lat.res/2, lat.u - lat.res/2, lat.res)
-
-	}
-
-	# Making grid
-
-	esquinas = c(
-        lon.l, lat.u, 
-        lon.r, lat.u, 
-        lon.r, lat.d, 
-        lon.l, lat.d, 
-        lon.l, lat.u
-        ) 
-
-	malla_gri = 
-        matrix(
-			esquinas,
-			byrow = TRUE, 
-			ncol = 2
-            ) %>%
-		list() %>%
-		sf::st_polygon() %>%
-		sf::st_sfc(crs = scr)
-	
-	malla = 
+    # Create grid polygon
+    boundaries = matrix(corners, byrow = TRUE, ncol = 2) |>
+        list() |>
+        sf::st_polygon() |>
+        sf::st_sfc(crs = crs) |>
         sf::st_make_grid(
-			malla_gri, 
-			n = c( lengths(list(lon,lat)) ),
-			crs = scr,
-			what = 'polygons'
-            ) %>%
-		sf::st_sf(
-			"geometry" = .,
-			data.frame(
-				"ID" = 1:length(.),
-				"lon" = rep(lon, times = length(lat)),
-				"lat" = rep(lat, each = length(lon))
-			    )
-		    ) 
+            n = ndims,
+            crs = crs,
+            what = "polygons"
+        )
+    poly_grid = sf::st_sf(
+        "geometry" = boundaries,
+        data.frame("ID" = seq_along(boundaries))
+    )
 
-    # Export grid
+    if (export) sf::st_write(poly_grid, name)
+    return(poly_grid)
+}
 
-	if (export) sf::st_write(malla, name)
-
-	return(malla)
-
+# Finding number of dimensions
+dimensionSize = function(points) {
+    points = points |>
+        sf::st_coordinates() |>
+        as.data.frame()
+    nlat = diff(points$X)
+    nlat = length(nlat[nlat < mean(nlat)]) + 1
+    nlon = as.integer(nrow(points) / nlat)
+    return(c(nlon, nlat))
 }
