8406f448864a95cc707f63342e730f6ed3b66dd0
Dante Castro Garro <profe.paiche@gmail.com>
2023-10-22 17:58:35 +0200

    Cleaning up code

--- R/maskMaker.R
+++ R/maskMaker.R
@@ -2,7 +2,7 @@
 #'
 #' This function find all the grids that intersect a polygon and calculates the
 #' fraction of the grids that are inside of the polygon.
-#' 
+#'
 #' @param grid Grid to be intersected.
 #' @param shp Polygons that will be used to intersect the grids.
 #' @param key Numeric. Field that will be used to define the dimensions of output
@@ -18,179 +18,126 @@
 #'
 #' @export
 
-maskMaker = function(grid, shp, 
-                     format = "array", 
-                     key = NULL, atrib_c = NULL, 
-                     export = NULL, 
-                     weights = NULL, 
+maskMaker = function(grid, shp,
+                     format = "array",
+                     key = NULL, atrib_c = NULL,
+                     export = NULL,
+                     weights = NULL,
                      progress = TRUE) {
 
-	# Trow error if "lon" or "lat" doesn't exist
+    # Trow error if "lon" or "lat" doesn't exist
+    cn = colnames(grid)
+    if (!(any(cn == "lon") && any(cn == "lat"))) {
+        stop("Error: variables lon or lat not found")
+    }
 
-	cn = colnames(grid)
-	if ( !(any(cn == "lon") & any(cn == "lat")) ) {
-		stop("Error: variables lon or lat not found")
-	} 
+    # Area for each grid
+    grid$Area_T = sf::st_area(grid) |> as.numeric()
 
-	# Area for each grid
+    # Defining output type
+    if (format == "array") {
+        lon_n = unique(grid$lon) |> length()
+        lat_n = unique(grid$lat) |> length()
+        mascara = array(0, c(lon_n, lat_n, nrow(shp)))
+    } else if (format == "list") {
+        mascara = list()
+    } else {
+        stop("Invalid format")
+    }
 
-	grid = grid %>%
-		dplyr::mutate(Area_T = sf::st_area(.) %>% as.numeric())
-	
-	# Defining output type
-
-	if (format == "array") {
-
-		lon.n = unique(grid$lon) %>% length()
-		lat.n = unique(grid$lat) %>% length()
-		mascara = array(0, c(lon.n, lat.n, nrow(shp)))
-
-	} else if (format == "list") {
-
-		mascara = list()
-
-	} else {
-
-		stop("Invalid format")
-
-	}
-	
     # Mask for polygon to be intersect
-
-	total = nrow(shp)
-	if (progress) pb = txtProgressBar(min = 0, max = total, style = 3)
-	ps = c()
-
-	for (i in 1:total) {
-	
-		shp_sel = shp[i, ]
-		
+    total = nrow(shp)
+    if (progress) pb = txtProgressBar(min = 0, max = total, style = 3)
+    ps = c()
+    for (i in 1:total) {
+        shp_sel = shp[i, ]
         # Intersect and calculate the fraction of the grid
-
-		oldw = getOption("warn")
-		options(warn = -1)
-
-		inters = 
-            suppressMessages(suppressWarnings(
-				sf::st_intersection(grid, shp_sel)
-			))
-		#inters = suppressMessages(sf::st_intersection(grid, shp_sel)) %>%
-		inters = inters %>%
-			dplyr::mutate(
-				Area_P = sf::st_area(.) %>% as.numeric(),
-				Frac = round(Area_P/Area_T, 3)
-            )
-		
-		options(warn = oldw)
+        oldw = getOption("warn")
+        options(warn = -1)
+        intersection = suppressMessages(suppressWarnings(
+            sf::st_intersection(grid, shp_sel)
+        ))
+        #inters = suppressMessages(sf::st_intersection(grid, shp_sel)) %>%
+        intersection$Area_P = sf::st_area(intersection) |> as.numeric()
+        intersection$Frac = intersection$Area_P / intersection$Area_T
+        options(warn = oldw)
 
         # Atribute table (1/2)
-
-		inters_tab = inters %>%
-			dplyr::as_tibble() %>%
-			dplyr::select(ID:lat, Frac)
+        intersection_df = intersection |>
+            dplyr::as_tibble() |>
+            dplyr::select(ID, Frac)
 
         # Weights
-
-		ps[i] = sum(inters_tab$Frac)
+        ps[i] = sum(intersection_df$Frac)
 
         # Atribute table (2/2)
-
-		if (!is.null(atrib_c)) {
-
-			inters_new = dplyr::as_tibble(inters)
-			inters_new = inters_new[ ,atrib_c]
-			inters_tab = cbind(inters_tab, inters_new)
-			inters_tab$Peso = ps[i]
-
-		}
+        if (!is.null(atrib_c)) {
+            inters_new = dplyr::as_tibble(intersection)
+            inters_new = inters_new[, atrib_c]
+            inters_tab = cbind(inters_tab, inters_new)
+            inters_tab$Peso = ps[i]
+        }
 
         # Output object
+        if (format == "array") {
+            mask = numeric(lon_n * lat_n)
+            mask[inters_tab$ID] = inters_tab$Frac
+            mascara[, , i] = matrix(
+                mask,
+                nrow = lon_n,
+                ncol = lat_n,
+                byrow = FALSE
+            )
+        } else if (format == "list") {
+            mascara[[i]] = inters_tab
+        }
 
-		if (format == "array") {
-
-			mask = numeric(lon.n * lat.n)
-			mask[inters_tab$ID] = inters_tab$Frac
-			mascara[ , ,i] = matrix(
-				mask, 
-				nrow = lon.n, 
-				ncol = lat.n, 
-				byrow = F
-			)
-
-		} else if (format == "list"){
-
-			mascara[[i]] = inters_tab
-
-		}
-
-		if (progress) setTxtProgressBar(pb, i)
-	
-	}
+        if (progress) setTxtProgressBar(pb, i)
+    }
 
     # Export weigth table
+    if (!is.null(weights)) {
+        shp_tab = as.data.frame(shp[, atrib_c]) |>
+            dplyr::mutate(Peso = ps) |>
+            dplyr::select(-geometry) |>
+            dplyr::as_tibble()
+        write.csv(shp_tab, weights, row.names = FALSE)
+    }
 
-	if (!is.null(weights)) {
-
-		shp_tab = as.data.frame(shp[ ,atrib_c]) %>%
-			mutate(Peso = ps) %>%
-			dplyr::select(-geometry) %>%
-			as_tibble()
-
-		write.csv(shp_tab, weights, row.names = FALSE)
-
-	}
-	
     # Output file as netCDF
-
-	if (!is.null(export) & format == "array" & !is.null(key)) {
-
-		# Definiendo dimensiones y variables
-		key.val = as.data.frame(shp)[ ,key]
-		londim = ncdim_def("lon", "degrees_east_west", unique(grid$lon))
-		latdim = ncdim_def("lat", "degrees_north", unique(grid$lat))
-		disdim = ncdim_def(key, key, as.integer(key.val),
-			longname = 'Field used in intersection')
-		
-		fillmissval = -999.9
-		
-		mask_def = ncvar_def(
-			name = "mask",
-			units = "fraction",
-			dim = list(londim, latdim, disdim),
-			missval = fillmissval, 
-			longname = "Mask"
+    if (!is.null(export) & format == "array" & !is.null(key)) {
+        # Definiendo dimensiones y variables
+        fillmissval = -999.9
+        key_val = as.data.frame(shp)[, key]
+        londim = ncdf4::ncdim_def("lon", "degrees_east_west", unique(grid$lon))
+        latdim = ncdf4::ncdim_def("lat", "degrees_north", unique(grid$lat))
+        disdim = ncdf4::ncdim_def(key, key, as.integer(key_val), longname = "Field used in intersection")
+        mask_def = ncdf4::ncvar_def(
+            name = "mask",
+            units = "fraction",
+            dim = list(londim, latdim, disdim),
+            missval = fillmissval,
+            longname = "Mask"
         )
 
-		peso_def = ncvar_def(
-			name = "weight",
-			units = "proportion",
-			dim = list(disdim),
-			missval = fillmissval,
-			longname = "Weights of each polygon based on intersected grids"
-        )
-		
-		ncout = nc_create(
-			filename = export,
-			vars = list(mask_def, peso_def),
-			force_v4 = T
-        )
-		
-		ncvar_put(
-			nc = ncout,
-			varid = mask_def,
-			vals = mascara
+        peso_def = ncdf4::ncvar_def(
+            name = "weight",
+            units = "proportion",
+            dim = list(disdim),
+            missval = fillmissval,
+            longname = "Weights of each polygon based on intersected grids"
         )
 
-		ncvar_put(
-			nc = ncout,
-			varid = peso_def,
-			vals = ps
+        ncout = ncdf4::nc_create(
+            filename = export,
+            vars = list(mask_def, peso_def),
+            force_v4 = TRUE
         )
-		
-		nc_close(ncout)
-		
-	}
 
-	return(mascara)
+        ncdf4::ncvar_put(nc = ncout, varid = mask_def, vals = mascara)
+        ncdf4::ncvar_put(nc = ncout, varid = peso_def, vals = ps)
+        ncdf4::nc_close(ncout)
+    }
 
+    return(mascara)
 }
